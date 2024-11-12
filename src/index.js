0b8eed2398743bcb1faac62bf43dba3deaadb59f
Watcharaphon Khanto <wpkhanto@icloud.com>
2024-11-12 21:59:12 +0700

    Create More Components

--- src/index.js
+++ src/index.js
@@ -47,17 +47,43 @@ const pizzaData = [
 ];
 
 function App() {
-    return <div><h1>Hello React!</h1><Pizza /></div>;
+    return(
+        <div>
+            <Header />
+            <Menu />
+            <Footer />
+        </div>
+    )
+}
+
+function Header() {
+    return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+    return (
+        <div>
+            <Pizza />
+            <Pizza />
+            <Pizza />
+        </div>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>{new Date().toLocaleTimeString()}. We're currently open</footer>
+    )
 }
 
 function Pizza() {
     return (
-    <div>
-        <img src="pizzas/spinaci.jpg" alt="Pizza spinaci"/>
-        <h2>Pizza Spinaci</h2>
-        <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-    )
+        <div>
+            <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
+            <h2>Pizza Spinaci</h2>
+            <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+        </div>
+    );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
