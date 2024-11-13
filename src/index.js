1ffe3e9fd7776f9b6afb7627378a8c499d8e4981
Watcharaphon Khanto <wpkhanto@icloud.com>
2024-11-13 21:00:09 +0700

    Conditional Rendering With ... and Destructuring Props

--- src/index.js
+++ src/index.js
@@ -66,15 +66,33 @@ function Header() {
 }
 
 function Menu() {
+    const pizzas = pizzaData;
+    // const pizzas = [];
+    const numPizzas = pizzas.length;
+
     return (
         <div className="menu">
             <h2>Our menu</h2>
 
-            <ul className="pizzas">
-                {pizzaData.map((pizza) => (
-                    <Pizza pizzaObj={pizza} key ={pizza.name}/>
-                ))}
-            </ul>
+            {numPizzas > 0 ? (
+                <>
+                    <p>
+                        Authentic Italian cuisine. 6 creative dishes to choose
+                        from. All from our stone oven, all organic, all
+                        delicious.
+                    </p>
+
+                    <ul className="pizzas">
+                        {pizzas.map((pizza) => (
+                            <Pizza pizzaObj={pizza} key={pizza.name} />
+                        ))}
+                    </ul>
+                </>
+            ) : (
+                <p>
+                    We're still working on our menu. Please come back later :)
+                </p>
+            )}
 
             {/* <Pizza
                 name="Pizza Spinaci"
@@ -92,15 +110,16 @@ function Menu() {
     );
 }
 
-function Pizza(props) {
-    console.log(props);
+function Pizza({ pizzaObj }) {
+    // if (pizzaObj.soldOut) return null;
+
     return (
-        <li className="pizza">
-            <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+        <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+            <img src={pizzaObj.photoName} alt={pizzaObj.name} />
             <div>
-                <h3>{props.pizzaObj.name}</h3>
-                <p>{props.pizzaObj.ingredients}</p>
-                <span>{props.pizzaObj.price}</span>
+                <h3>{pizzaObj.name}</h3>
+                <p>{pizzaObj.ingredients}</p>
+                <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
             </div>
         </li>
     );
@@ -116,10 +135,31 @@ function Footer() {
     // if (hour >= openHour && hour <= closeHour) alert("We're currently open!")
     // else alert("Sorry we're closed")
 
+    // if (!isOpen) return <p>CLOSED</p>;
+
     return (
         <footer className="footer">
-            {new Date().toLocaleTimeString()}. We're currently open
+            {isOpen ? (
+                <Order closeHour={closeHour} openHour={openHour} />
+            ) : (
+                <p>
+                    We're happy to welcome you between {openHour}:00 and{" "}
+                    {closeHour}:00
+                </p>
+            )}
         </footer>
+    );
+}
+
+function Order({ closeHour, openHour }) {
+    return (
+        <div className="order">
+            <p>
+                We're open from {openHour}:00 to {closeHour}:00. Come visit us
+                or order online.
+            </p>
+            <button className="btn">Order</button>
+        </div>
     );
 }
 
